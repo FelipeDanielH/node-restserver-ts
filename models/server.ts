2342e6b4b28ce857453a723d87c3f23c1953ef58
Felipe <felipe.daniel.henriquez@gmail.com>
2024-11-25 14:58:51 -0300

    Agrega rutas y controlador

--- models/server.ts
+++ models/server.ts
@@ -1,16 +1,26 @@
 import express, { Application }  from "express";
+import userRouter from "../routes/usuarios";
 
 
 class Server {
 
     private app: Application;
     private port: string;
-
+    private apiPaths = {
+        usuarios: '/api/usuarios'
+    }
 
     constructor() {
         this.app = express();
-
         this.port = process.env.PORT || '8000';
+
+        // Definicion de rutas
+        this.routes();
+    }
+
+
+    routes(){
+        this.app.use( this.apiPaths.usuarios, userRouter  )
     }
 
     listen(){
@@ -18,6 +28,8 @@ class Server {
             console.log('Servidor corriendo en puerto ' + this.port );
         })
     }
+
+
 }
 
 export default Server;
